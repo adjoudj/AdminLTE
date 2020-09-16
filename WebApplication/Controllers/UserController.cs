3f01da96ef4a19c05e3300774bf051f6f26058e5
Administrateur <Administrateur@enap.com>
2020-09-16 15:37:42 +0100

    Ajoutez des fichiers projet.

---
+++ WebApplication/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace NewIdentity.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        // GET: Test  
+        /// <summary>  
+        /// Identity With Role only for Identity action method.  
+        /// </summary>  
+        /// <returns></returns>  
+        ///   
+        [Authorize(Roles = "User")] // Admin only can access  
+        public ActionResult Identity()
+        {
+            return Content("We are using Identity");
+        }
+        [Authorize(Roles = "DPQ")] // Admin only can access  
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult NonIdentiy()
+        {
+            return Content("We are not using Identity");
+        }
+    }
+}
